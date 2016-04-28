c6be9d37529321849358ec9dcab1579d81219026
Daniel Topp <dmtopp@gmail.com>
2016-04-27 19:01:35 -0500

    working on synthesizer params

--- sockets_drum_pads/app.js
+++ sockets_drum_pads/app.js
@@ -1,4 +1,97 @@
-var context = new AudioContext();
+var context      = new AudioContext(),
+    masterVolume = context.createGain();
+
+
+
+// set up querty-hancock keyboard
+var keyboard = new QwertyHancock({
+  id:      'keyboard',
+  width:   800,
+  height:  200,
+  octaves: 3
+})
+
+// object to store active oscillators
+var oscillators = {};
+
+// context.destination ~= master out
+// chains look roughly like osc -> effects/filters -> masterVolume -> context.destination aka speakers or headphones
+masterVolume.gain.value = 0.3;
+masterVolume.connect(context.destination);
+
+function Oscillator(context, wave, freq){
+  this.osc = context.createOscillator();
+  this.osc.type = wave;
+  this.setFrequency(freq);
+  this.osc.start(0);
+
+  this.input = this.osc;
+  this.output = this.osc;
+
+}
+
+keyboard.data = {
+  osc1: 'square',
+  osc2: 'triangle',
+  attack: 0.001,
+  release: 0.5,
+  filterType: 'lowpass',
+  filterCutoff: 2000,
+  osc1Detune: -5,
+  osc2Detune: 5
+}
+
+
+keyboard.keyDown = function (note, frequency) {
+  var osc = context.createOscillator();
+  var osc2 = context.createOscillator();
+  var oscVolume = context.createGain();
+  var filter = context.createBiquadFilter();
+
+  osc.start(context.currentTime);
+  osc2.start(context.currentTime);
+
+  osc.type = keyboard.data.osc1;
+  osc2.type = keyboard.data.osc2;
+
+  // set gain for both oscillators
+  // start gain at 0 and then ramp up over attack time
+  oscVolume.gain.value = 0;
+  oscVolume.gain.setTargetAtTime(1, context.currentTime, keyboard.data.attack);
+
+  osc.frequency.value  = frequency;
+  osc2.frequency.value = frequency;
+  osc.detune.value = keyboard.data.osc1Detune;
+  osc2.detune.value = keyboard.data.osc2Detune;
+
+  filter.type = keyboard.data.filterType;
+  filter.frequency.value = keyboard.data.filterCutoff;
+
+
+
+  // (osc, osc2) -> oscVolume -> master out
+  osc.connect(oscVolume);
+  osc2.connect(oscVolume);
+  oscVolume.connect(filter);
+  filter.connect(masterVolume);
+
+  oscObject = {
+    voices: [osc],
+    volume: oscVolume
+  }
+
+  oscillators[frequency] = oscObject;
+
+}
+
+keyboard.keyUp = function (note, frequency) {
+
+  oscillators[frequency].volume.gain.setTargetAtTime(0, context.currentTime, keyboard.data.release);
+
+}
+
+// code for drum pads
+// ========================================
 
 var sounds = [{
                 url: "http://dab1nmslvvntp.cloudfront.net/wp-content/uploads/2014/08/1407409274kick.wav",
@@ -46,10 +139,32 @@ function addAudioProperties(object) {
     object.s = s;
   }
 }
+// ========================================
+//  end code for drum pads
+
+
+
 
 window.onload = function() {
 
+
+// this is the part you are working on !
+// ========================================
+  var keyParams = document.querySelectorAll('.keyboardParams');
+
+  for (var i = 0; i < keyParams.length; i++){
+    keyParams[i].addEventListener('change', function(){
+      console.log(this.name);
+      console.log(this.value);
+      keyboard.data[this.name] = this.value + 0.001;
+    })
+  }
+// this is the part you are working on !
+// ========================================
+
   console.log('the window is loaded and so am i....');
+
+  console.log(keyboard.data);
 
   var pads        = document.querySelectorAll('#soundpad div'),
       sockets     = io.connect(),
@@ -83,7 +198,7 @@ window.onload = function() {
 
   sockets.on('playSound', function(buttonId){
     sounds[buttonId].play();
-    console.log('wow! a sound!');
+    // console.log('wow! a sound!');
   })
 
   // event listener for our chat form
